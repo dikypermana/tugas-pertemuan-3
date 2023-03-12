013010ef613a4a47c8dee4562503f10595e0fdf0
dikypermana <dikypermana50@gmail.com>
2023-03-12 15:49:29 +0700

    first commit

---
+++ DataDiri.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+   
+class DataDiri {
+  public static void main(String args[]){
+            
+    System.out.println("##  Program Java Biodata Mahasiswa ##");
+    System.out.println("========================================");
+    System.out.println();
+    System.out.println("# Data Mahasiswa #");
+    System.out.println("==================");
+    System.out.println("Nama: Diky Permana");
+    System.out.println("NPM: 2022320005");
+    System.out.println("Fakultas: FI22B");
+    System.out.println("Jurusan: Sistem Informasi" );
+    System.out.println("Kota Asal: Bekasi");
+    System.out.println("Alamat: Graha Harapan Regency Blok G3 No. 11 A");
+  }
+}
